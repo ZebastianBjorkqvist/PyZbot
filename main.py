652515b2ee2597a9b5491d3fb5a9ba18ca90bf3b
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-10-20 12:19:35 +0200

    Fixed importing and adding of Cogs

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # main.py
 import os
-import Cogs
 from discord.ext import commands
 from dotenv import load_dotenv
+from Cogs.Greetings import Greetings
+from Cogs.RandomCommands import RandomCommands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 
 bot = commands.Bot(command_prefix='$')
 
@@ -16,7 +16,7 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-bot.add_cog(Cogs.Greetings(bot))
-bot.add_cog(Cogs.RandomCommands(bot))
+bot.add_cog(Greetings(bot))
+bot.add_cog(RandomCommands(bot))
 
 bot.run(TOKEN)
