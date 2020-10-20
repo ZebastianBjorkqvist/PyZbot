110c2f59fbe4ae2b0adc07f6894ad824da226cab
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-10-20 12:14:58 +0200

    Improved readability of Cogs import and adding

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # main.py
 import os
-from Cogs.RandomCommands import RandomCommands
+import Cogs
 from discord.ext import commands
 from dotenv import load_dotenv
-from Cogs.Greetings import Greetings
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,7 +16,7 @@ async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
 
-bot.add_cog(Greetings(bot))
-bot.add_cog(RandomCommands(bot))
+bot.add_cog(Cogs.Greetings(bot))
+bot.add_cog(Cogs.RandomCommands(bot))
 
 bot.run(TOKEN)
