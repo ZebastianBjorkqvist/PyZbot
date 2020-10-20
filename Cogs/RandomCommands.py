631de4cc9a653d1174af8908f763ee4bcc9446c8
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2020-10-20 12:01:20 +0200

    Updated piggy command to handle empty response from Pixabay

--- Cogs/RandomCommands.py
+++ Cogs/RandomCommands.py
@@ -1,4 +1,5 @@
 import json
+import os
 import random
 from urllib.request import urlopen
 import requests
@@ -10,6 +11,22 @@ class RandomCommands(commands.Cog):
         self.bot = bot
         self._last_member = None
 
+    async def get_piggie_image(self):  # We need to have this in a separate function so we can call it if the hits in
+        # the request is empty. Seems to happen approx 1 in 10 times
+        page = random.randint(1, 10)
+        image = random.randint(1, 20)
+
+        url = 'https://pixabay.com/api/'
+        params = {'key': os.getenv('PIXABAY_TOKEN'), 'q': 'guinea pig', 'per_page': 50, 'page': page}
+
+        r = requests.get(url=url, params=params)
+        data = r.json()
+
+        if len(data['hits']) < 1:
+            return await self.get_piggie_image()
+
+        return data['hits'][image]['webformatURL']
+
     @commands.command()
     async def dog(self, ctx):
         url = "https://dog.ceo/api/breeds/image/random"
@@ -20,13 +37,4 @@ class RandomCommands(commands.Cog):
 
     @commands.command()
     async def piggy(self, ctx):
-        page = random.randint(1, 10)
-        image = random.randint(1, 20)
-
-        url = 'https://pixabay.com/api/'
-        params = {'key': '18241563-32b85cbf1a4e1ca5932c8b396', 'q': 'guinea pig', 'per_page': 50, 'page': page}
-
-        r = requests.get(url=url, params=params)
-        data = r.json()
-        print(data)
-        await ctx.send(data['hits'][image]['webformatURL'])
+        await ctx.send(await self.get_piggie_image())
