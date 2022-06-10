91b323ca2c8190b499af944d8778dd63a965bc62
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2022-06-10 09:21:51 +0200

    Added logging with verbose level INFO to console

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 # main.py
 import os
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 from Cogs.Greetings import Greetings
 from Cogs.RandomCommands import RandomCommands
+import logging
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+logging.basicConfig(level=logging.INFO)
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='!')
 
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
     print(f'{bot.user.name} has connected to Discord!')
+
 
 
 bot.add_cog(Greetings(bot))
 bot.add_cog(RandomCommands(bot))
-
 bot.run(TOKEN)
