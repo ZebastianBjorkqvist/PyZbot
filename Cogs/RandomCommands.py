7d2c1593eb30e8d9744ff8ec634545ab57a27278
ZebastianBjorkqvist <zebastian.bjorkqvist@gmail.com>
2022-06-10 09:22:24 +0200

    Fixed comments and added guess command to guess random number

--- Cogs/RandomCommands.py
+++ Cogs/RandomCommands.py
@@ -11,8 +11,7 @@ class RandomCommands(commands.Cog):
         self.bot = bot
         self._last_member = None
 
-    async def get_piggie_image(self):  # We need to have this in a separate function so we can call it if the hits in
-        # the request is empty. Seems to happen approx 1 in 10 times
+    async def get_piggie_image(self):
         page = random.randint(1, 10)
         image = random.randint(1, 20)
 
@@ -29,7 +28,7 @@ class RandomCommands(commands.Cog):
 
     @commands.command()
     async def dog(self, ctx):
-        """Sends an almost as beautiful image"""  # This adds the text to the $help command
+        """Sends an almost as beautiful image"""  # Text for !help command
         url = "https://dog.ceo/api/breeds/image/random"
         json_response = urlopen(url)
         data = json.loads(json_response.read())
@@ -38,14 +37,23 @@ class RandomCommands(commands.Cog):
 
     @commands.command()
     async def piggy(self, ctx):
-        """Sends a beautiful image"""  # This adds the text to the $help command
+        """Sends a beautiful image"""  # Text for !help command
         await ctx.send(await self.get_piggie_image())
 
-    @commands.command(help='Rolls dice. $roll_dice {number of dice} {number of sides}')  # This also adds the text to
-    # the $help command
+    @commands.command()
     async def roll_dice(self, ctx, number_of_dice: int, number_of_sides: int):
+        """Rolls dice. !roll_dice {number of dice} {number of sides}"""  # Text for !help command
         dice = [
             str(random.choice(range(1, number_of_sides + 1)))
             for _ in range(number_of_dice)
         ]
         await ctx.send(', '.join(dice))
+
+    @commands.command()
+    async def guess(self, ctx, number: int):
+        """ Guess a random number from 1 to 6. """
+
+        value = random.randint(1, 6)
+
+        emoji = '\N{WHITE HEAVY CHECK MARK}' if value == number else '\N{CROSS MARK}'
+        await ctx.message.add_reaction(emoji)
